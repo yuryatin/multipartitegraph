e5824236ebc6d417089cb117156a0392f0eba616
Alexander Yuryatin <u_alal@yahoo.com>
2022-09-09 23:59:33 +0300

    new minor version with corrected setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'multipartitegraph',
     packages = ['multipartitegraph'],
-    version = '0.0.1',
+    version = '0.0.3',
     license = 'MIT',
     description = 'This package draws multipartite graph with fixed levels from a list of matrix dataframes',
     url = 'https://github.com/yuryatin/multipartitegraph',
-    download_url = 'https://github.com/yuryatin/multipartitegraph/archive/refs/tags/v0.0.1.tar.gz',
+    download_url = 'https://github.com/yuryatin/multipartitegraph/archive/refs/tags/v0.0.3.tar.gz',
     keywords = ['multipartite', 'graph', 'fixed', 'levels', 'plotting'],
     classifiers = [],
-    install_requires = ['copy','string','numpy','pandas','matplotlib']
+    install_requires = ['numpy','pandas','matplotlib']
 )
